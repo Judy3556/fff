49fabb201648f5fa66b83ee755392d995523b0f0
m305-14\w401 <dddd>
2018-03-08 17:31:48 +0800

    create a vs work

---
+++ work/work/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Long long ago, there is a man named pan. And then nothing happened. This story is finished.");
+            Console.WriteLine("hello");
+            Console.Read();
+        }
+    }
+}
